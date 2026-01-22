e9f5d5fb36012cf8f6a09fe97f1383c920a31c50
Markus Sommer <markus.sommer@fullhaus.de>
2026-01-22 11:40:23 +0100

    feat: ordered types by sebo like (#17)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 /*
  * This file is part of the FULLHAUS PHP-CS-Fixer configuration.
  *
- * (c) 2024-2026 FULLHAUS GmbH
+ * (c) 2026 FULLHAUS GmbH
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace FULLHAUS\CodingStandards;
+$config = FULLHAUS\CodingStandards\CsFixerConfig::create();
+$config->getFinder()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
 
-interface CsFixerConfigInterface
-{
-    public function __construct(string $name);
-}
+return $config;
---
+++ tests/README.md
@@ -0,0 +1,19 @@
+# Tests
+
+This directory contains comprehensive tests for the FULLHAUS PHP-CS-Fixer configuration.
+
+## Running Tests
+
+### Install Dependencies
+
+First, install the required dependencies:
+
+```bash
+composer install
+```
+
+### Run All Tests
+
+```bash
+vendor/bin/phpunit
+```
